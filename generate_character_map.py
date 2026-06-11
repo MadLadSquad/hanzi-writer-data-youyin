9cf306a3802f7004a19ca84b9b39721b725ffa81
Madman10K <stanv.contact176@gmail.com>
2026-06-12 02:04:04 +0300

    Add full character map that contains all character data as well

--- generate_character_map.py
+++ generate_character_map.py
@@ -20,5 +20,21 @@ def main():
     with open(output_filename, "w", encoding="utf-8") as f:
         json.dump(files, f, ensure_ascii=False)
 
+    # Compile character-map-full data
+    full_data = []
+    for char_name in files:
+        char_file_path = os.path.join(data_dir, f"{char_name}.json")
+        with open(char_file_path, "r", encoding="utf-8") as char_file:
+            content = json.load(char_file)
+        full_data.append({
+            "char": char_name,
+            "content": content
+        })
+
+    # Save the resulting array to character-map-full.json in minified format
+    output_full_filename = "character-map-full.json"
+    with open(output_full_filename, "w", encoding="utf-8") as f:
+        json.dump(full_data, f, ensure_ascii=False, separators=(",", ":"))
+
 if __name__ == "__main__":
     main()
